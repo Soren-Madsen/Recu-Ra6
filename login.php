177fad10e817cebbbe0aafc8a530f42db8cb8b84
Adri <adristucom@gmail.com>
2025-03-31 09:55:57 +0200

    aaaaaa

--- login.php
+++ login.php
@@ -8,7 +8,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
-    <title>CineFest Catalunya</title>
+    <title>CineFest Catalunyaaaa</title>
 </head>
 
 
