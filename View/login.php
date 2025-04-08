e363c868543a6fbb2299de0f04bd692939565ae5
aniol <aniolnicolauroig@gmail.com>
2025-04-08 08:24:31 +0200

    un poco de css

--- View/login.php
+++ View/login.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -12,42 +11,43 @@
 
 
 <body>
-<?php
+    <?php
 
-?>
-    <div id="T_IniciaSession">
-        <h1>INICIA SESIÓN</h1>
-    </div>
-    <div id="T_EmailContraseña">
-        <h2>Introduce el correo electrónico y la contraseña de tu cuenta CFC.</h2>
-    </div>
-    <div id="rectangle">
-        <form action="" method="POST">
-            <!-- DIRECCION DE CORREO ELECTRONICO -->
+    ?>
+    <div id="LoginCentred">
+        <div id="T_IniciaSession">
+            <h1>INICIA SESIÓN</h1>
+        </div>
+        <div id="T_EmailContraseña">
+            <h2>Introduce el correo electrónico y la contraseña de tu cuenta CFC.</h2>
+        </div>
+        <div id="rectangle">
+            <form action="" method="POST">
+                <!-- DIRECCION DE CORREO ELECTRONICO -->
 
-            <label for="email">
-                <h2>Dirección de correo electrónico:</h2>
-            </label>
-            <div id="TextBox">
-                <input class="inputbox" type="text" name="email" value="<?php echo isset($_POST['email']) ? htmlspecialchars($_POST['email']) : ''; ?>">
-            </div>
-
-            <!-- PASSWORD -->
-            <label for="password">
-                <h2>Contraseña:</h2>
+                <label for="email">
+                    <h2>Dirección de correo electrónico:</h2>
+                </label>
                 <div id="TextBox">
+                    <input class="inputbox" type="text" name="email" value="<?php echo isset($_POST['email']) ? htmlspecialchars($_POST['email']) : ''; ?>">
+                </div>
 
-            </label>
+                <!-- PASSWORD -->
+                <label for="password">
+                    <h2>Contraseña:</h2>
+                    <div id="TextBox">
+
+                </label>
                 <input class="inputbox" type="password" name="password" pattern="/^[a-zA-Z0-9!@#\$%\^\&*_=+-]{8,12}$/g" value="<?php echo isset($_POST['password']) ? htmlspecialchars($_POST['password']) : ''; ?>" /><br>
-            </div>
+        </div>
 
-            <h3>
-                <link href="" value="">¿Has olvidado tu contraseña?
-            </h3>
-            <!-- LOGIN -->
-            <input type="submit" id="login" value="Inicia sesión" />
-            <!-- SIGNIN -->
-            <input type="submit" id="signin" value="Registrarme" />
+        <h3>
+            <link href="" value="">¿Has olvidado tu contraseña?
+        </h3>
+        <!-- LOGIN -->
+        <input type="submit" id="login" value="Inicia sesión" />
+        <!-- SIGNIN -->
+        <input type="submit" id="signin" value="Registrarme" />
 
         </form>
     </div>
