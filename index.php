2e1cb616b20dc2bf6e6572ea7449df8c757fb182
itsfirestorm <marcms244@gmail.com>
2025-04-08 10:39:46 +0200

    Added transparent logos, implemented index.html and css

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="/View/files/style/navbar.css"
+    <link rel="stylesheet" href="/View/style/style.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cine Fest Catalunya // Página principal</title>
+</head>
+<body>
+    <header>
+        <ul id="navbar">
+            <img src="/View/files/img/CineFestCatalunya-dark.png">
+            <a href="#" class="nav-link">Home</a>
+            <a href="#" class="nav-link">Eventos</a>
+            <a href="#" class="nav-link">Calendario</a>
+            <a href="#" class="nav-link">Noticias</a>
+            <a href="#" class="nav-link">Foros</a>
+        </ul>
+    </header>
+    <events>
+        <p>Eventos</p>
+        <event class="event-slideshow"></event>
+        <event class="event-slideshow"></event>
+        <event class="event-slideshow"></event>
+        <event class="event-slideshow"></event>
+        <event class="event-slideshow"></event>
+        <event class="event-slideshow"></event>
+        <event id="main-event">
+    </events>
+    <div id="sponsors">
+
+    </div>
+    <footer>
+        <div id="connections"></div>
+        <div id="legal-stuff"></div>
+    </footer>
+</body>
+</html>
