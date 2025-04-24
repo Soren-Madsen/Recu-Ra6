6d6812463cce9c8226a6e05360a47ea68d848329
aniol <aniolnicolauroig@gmail.com>
2025-04-24 12:55:22 +0200

    css de login a medias

--- View/login.php
+++ View/login.php
@@ -1,13 +1,14 @@
 <!DOCTYPE html>
 <html lang="en">
-<link rel="style" href="style.css">
 
 <head>
+    <link rel="stylesheet" href="./View/files/style/style.css">
+    <link rel="stylesheet" href="./View/files/style/login.css">
+
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
-    <title>CineFest Catalunya</title>
-    
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <title>CineFest Catalunya // Login</title>
 </head>
 
 <body>
