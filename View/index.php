c00d4780e65b828442d2024624ed790dde61f360
aniol <aniolnicolauroig@gmail.com>
2025-04-28 08:59:42 +0200

    errores causados por el moviemientos de archivos

--- View/index.php
+++ View/index.php
@@ -1,8 +1,8 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <link rel="stylesheet" href="./View/files/style/navbar.css">
-    <link rel="stylesheet" href="./View/files/style/style.css">
+    <link rel="stylesheet" href="./files/style/navbar.css">
+    <link rel="stylesheet" href="./files/style/style.css">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cine Fest Catalunya // Página principal</title>
